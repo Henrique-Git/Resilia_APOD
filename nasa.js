0931307f6981fe42854098e475ad4241ca7a792f
Henrique Medeiros <rique.medeirosneves@gmail.com>
2021-10-01 00:04:48 -0300

    Estilização Final: Finalização do Projeto

--- nasa.js
+++ nasa.js
@@ -1,5 +1,7 @@
 let titulo = $("#titulo");
 let imagem = $("#imagem");
+let diVideo = $('#div-video');
+let video = $('#video');
 let explicacao = $("#explicacao");
 let botao = $("#botao");
 
@@ -7,9 +9,24 @@ $.ajax({
     url: `https://api.nasa.gov/planetary/apod?api_key=p1ksHdXji6yr7QuCmYFbmtoPcmE5VlByhjOs5jpD`,
 
     success(result){
-    
+
+         //condição para quando for imagem, adiciona uma classe e remove outra
+         if (result.media_type == "image") {
+            diVideo.css({"display": `none`});
+            imagem.css({"display": `block`});
+            imagem.css({"background": `url(${result.url})`, "background-repeat": `no-repeat`, "background-size": `60% 100%`, "background-position": `center`});
+        }
+
+        //quando for vídeo trocar as condições
+        else {
+            imagem.css({"display": `none`});
+            diVideo.css({"display": `flex`});
+            video.attr('src', `${result.url}`)
+        }
+
         titulo[0].innerHTML = result.title;
-        imagem.css({"background": `url(${result.url})`, "background-repeat": `no-repeat`, "background-size": `50% 100%`, "background-position": `center`});
+
+        
         explicacao[0].innerHTML = result.explanation; 
 
         console.log(result);
@@ -23,9 +40,22 @@ function apod(){
     
     success(result){
         
+        //condição para quando for imagem, adiciona uma classe e remove outra
+         if (result.media_type == "image") {
+            diVideo.css({"display": `none`});
+            imagem.css({"display": `block`});
+            imagem.css({"background": `url(${result.url})`, "background-repeat": `no-repeat`, "background-size": `60% 100%`, "background-position": `center`});
+        }
+
+        //quando for vídeo trocar as condições
+        else {
+            imagem.css({"display": `none`});
+            diVideo.css({"display": `flex`});
+            video.attr('src', `${result.url}`)
+        }
+
         titulo[0].innerHTML = result.title;
 
-        imagem.css({"background": `url(${result.url})`, "background-repeat": `no-repeat`, "background-size": `50% 100%`, "background-position": `center`});  
         explicacao[0].innerHTML = result.explanation; 
 
         console.log(result);
